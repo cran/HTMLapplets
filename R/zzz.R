232aab633cad3d2dc939a30135d15da6fe95f2fe
Gregoire Thomas <Gregoire.Thomas@ugent.be>
2004-12-03 00:00:00 +0000

    version 0.1-1

---
+++ R/zzz.R
@@ -0,0 +1,15 @@
+# +++++++++++++++++++++++++++++++++++++++++++
+# name:        zzz.R
+# date:        2004/11/18
+# author:      Gregoire Thomas
+# email:       gregoire.thomas@ugent.be
+# description: 
+# copyright (c) VIB and Ghent University 2004
+# +++++++++++++++++++++++++++++++++++++++++++
+
+# ++++++ .First.lib +++++++++++++++++++++++++++++++++++++++++++
+.First.lib<-function(lib, pkg) {
+  require(R2HTML)
+}
+# -------------------------------------------------------------
+
